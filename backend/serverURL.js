da6a18f42e273ea635c735388b922014c57de93b
Rewe <kewyn.ryan@gmail.com>
2020-08-10 11:00:43 -0300

    Simple post form

---
+++ backend/serverURL.js
@@ -0,0 +1,8 @@
+// Backend variables to sync the urls of get/post requests from frontend
+const serverURL = 
+{
+    host: 'localhost',
+    port: 8081
+}
+
+module.exports = serverURL;
--- src/App/PostItem.tsx
+++ src/App/PostItem.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
+import backendURL from "../../backend/serverURL";
 
 export default function PostItem()
 {
+    console.log(backendURL.host);
     return (
         <div className='appPostItem'>
             <h1>Post item</h1>
+            <form id='itemForm' action={`http://${backendURL.host}:${backendURL.port}/postItem`} method='post'>
+                <input name='title' type='text' placeholder="Type the post's title..."/>
+                <input name='category' type='text' placeholder="Type the item's category..."/>
+                <input name='price' type='number' placeholder="Type the item's price..."/>
+            </form>
+                <button form='itemForm' type='submit'>Submit</button>
         </div>
     )
 }
