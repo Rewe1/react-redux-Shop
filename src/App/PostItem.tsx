ee4ed83bc4ea39b5da7204ee80b310761b37f19e
Rewe <kewyn.ryan@gmail.com>
2020-08-10 11:43:59 -0300

    Fixed form inputs and added some styling

--- src/App/PostItem.tsx
+++ src/App/PostItem.tsx
@@ -8,9 +8,14 @@ export default function PostItem()
         <div className='appPostItem'>
             <h1>Post item</h1>
             <form id='itemForm' action={`http://${backendURL.host}:${backendURL.port}/postItem`} method='post'>
-                <input name='title' type='text' placeholder="Type the post's title..."/>
-                <input name='category' type='text' placeholder="Type the item's category..."/>
-                <input name='price' type='number' placeholder="Type the item's price..."/>
+                <input name='title' type='text' placeholder="Type the post's title..." required/>
+                <select name='category' required>
+                    <option selected={true} disabled>Select a category...</option>
+                    <option value='Cups'>Cups</option>
+                    <option value='Clothes'>Clothes</option>
+                    <option value='Tools'>Tools</option>
+                </select>
+                <input name='price' type='text' pattern="[.0-9]{1,8}" placeholder="Type the item's price..." required />
             </form>
                 <button form='itemForm' type='submit'>Submit</button>
         </div>
