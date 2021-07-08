2a2df9b3e52ba22b653b2119f7b14283198b5434
Rewe <kewyn.ryan@gmail.com>
2021-07-08 13:01:58 -0300

    Simplified post-page

--- src/App/components/post-item-page/index.tsx
+++ src/App/components/post-item-page/index.tsx
@@ -4,11 +4,11 @@ import backendURL from "../../../../backend/serverURL";
 export default function PostItem()
 {
     return (
-        <div className='appPostItem'>
+        <div className='post-item-div'>
             <h1>Post item</h1>
-            <form id='itemForm' action={backendURL.postURL} method='post'>
-                <input name='title' maxLength={32} type='text' placeholder="Type the post's title..." required/>
-                <select name='category' required>
+            <form className='post-item-form' id='itemForm' action={backendURL.postURL} method='post'>
+                <input className='title-input' name='title' maxLength={32} type='text' placeholder="Type the post's title..." required/>
+                <select className='category-input' name='category' required>
                     <option value='Clothes'>Clothes</option>
                     <option value='Tools'>Tools</option>
                     <option value='Hobby'>Hobby</option>
@@ -17,10 +17,10 @@ export default function PostItem()
                     <option value='Electronics'>Electronics</option>
                     <option value='Other'>Other</option>
                 </select>
-                <textarea form='itemForm' name='description' maxLength={128} placeholder="Type the item's description..." required/>
-                <input name='price' maxLength={8} type='text' pattern="[.0-9]{1,8}" placeholder="Type the item's price..." required />
+                <textarea className='description-textarea' form='itemForm' name='description' maxLength={128} placeholder="Type the item's description..." required/>
+                <input className='price-input' name='price' maxLength={8} type='text' pattern="[.0-9]{1,8}" placeholder="Type the item's price..." required />
             </form>
-                <button form='itemForm' type='submit'>Post item</button>
+                <button className='submit-button' form='itemForm' type='submit'>Post item</button>
         </div>
     )
 }
