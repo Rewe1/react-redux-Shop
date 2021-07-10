5d3b811d634964f7d1872d9f72710530f564881d
Rewe <kewyn.ryan@gmail.com>
2021-07-10 01:21:15 -0300

    Nav responds to login

--- src/App/components/app-nav/index.tsx
+++ src/App/components/app-nav/index.tsx
@@ -8,21 +8,29 @@ export default function Nav()
     const state: tRootState = useSelector((state: tRootState) => state)
 
     return (
-        <nav className='app-nav'>
+        <header className='app-header'>
             <img className='app-logo' src='https://via.placeholder.com/48'/>
-            <Link className='home-page-a' to='/'>
-                Shop
-            </Link>
-            <Link className='home-page-a' to='/register-account'>
-                Register
-            </Link>
-            <Link className='home-page-a' to='/login'>
-                Login
-            </Link>
+            <nav className='app-nav'>
+                <Link className='home-page-a' to='/'>
+                    Shop
+                </Link>
+                {
+                    !state.account._id.length &&
+                    <Link className='home-page-a' to='/register-account'>
+                        Register
+                    </Link>
+                }
+                {
+                    !state.account._id.length &&
+                    <Link className='home-page-a' to='/login'>
+                        Login
+                    </Link>
+                }
+            </nav>
             <Link className='cart-page-a' to='/cart'>
                 <img className='cart-a-img' src={cartImg}/>
                 <span className='cart-items-number'>{state.cartItems.items.length}</span>
             </Link>
-        </nav>
+        </header>
     )
 }
