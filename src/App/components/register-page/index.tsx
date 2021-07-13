4639e4b8618295060ffa11fc771971b7e5d48654
Rewe <kewyn.ryan@gmail.com>
2021-07-13 11:40:06 -0300

    Added confirm password and stylized a bit more

--- src/App/components/register-page/index.tsx
+++ src/App/components/register-page/index.tsx
@@ -9,6 +9,7 @@ export default function RegisterPage()
 {
     let [isVendor, setVendor] = useState(false)
 
+    let [isPassEqual, setPassEqual] = useState(true)
     let [isEmailUsed, setEmailUsed] = useState(false)
     let [success, setSuccess] = useState(false)
 
@@ -16,7 +17,15 @@ export default function RegisterPage()
 
     let postRegister = async () => 
     {
+        setSuccess(false)
+        setEmailUsed(false)
+        setPassEqual(true)
+
         let formData = new FormData(document.getElementById('register-form') as HTMLFormElement)
+
+        if(formData.get('password') != formData.get('confirm-password'))
+            return setPassEqual(false)
+
         let res = await fetch(`${serverURL.url}/${serverURL.accounts.registerPath}`, {
             headers:{          
                 'Accept': 'application/json',
@@ -49,6 +58,7 @@ export default function RegisterPage()
         if(res.status === 200)
         {
             setEmailUsed(false)
+            setPassEqual(true);
             dispatch(stateRoot.actions.account.login(await res.json()))
             setSuccess(true)
         }
@@ -66,36 +76,50 @@ export default function RegisterPage()
                     isEmailUsed &&
                     <span className='error-span'>Email is already in use</span>
                 }
-                <div className='account-type-div'>
-                    <span>Você é</span>
-                    <label>
-                        Consumidor
-                        <input type='radio' name='account-type' onClick={() => setVendor(false)} value='false' defaultChecked></input>
-                    </label>
-                    <label>
-                        Vendedor
-                        <input type='radio' name='account-type' onClick={() => setVendor(true)} value='true'></input>
-                    </label>
-                </div>
+                {
+                    !isPassEqual &&
+                    <span className='error-span'>Passwords don't match</span>
+                }
                 <form className='register-form' id='register-form' method='post'>
-                    {
-                        isVendor &&
-                        <input type='text' name='CNPJ' className='cnpj-input' placeholder='CNPJ'></input>
-                    }
-                    <input type='email' name='email' className='email-input' placeholder='Email'></input>
-                    <input type='password' name='password' className='password-input' placeholder='Password'></input>
-                    <input type='password' className='password-input' placeholder='Confirm password'></input>
+                    <div className='account-id-div'>
+                        <input type='email' name='email' className='email-input' placeholder='Email' required></input>
+                        <input type='password' name='password' className='password-input' placeholder='Password' required></input>
+                        <input type='password' name='confirm-password' className='password-input' placeholder='Confirm password' required></input>
+                    </div>
 
-                    <input type='text' name='phone' className='phone-input' placeholder='Telefone'></input>
-                    <input type='text' name='whatsapp' className='whatsapp-input' placeholder='Whatsapp'></input>
+                    <div className='account-info-div'>
+                        <div className='account-type-div'>
+                            <span>Você é</span>
+                            <div>
+                                <label>
+                                    consumidor
+                                    <input type='radio' name='account-type' onClick={() => setVendor(false)} value='false' defaultChecked></input>
+                                </label>
+                                <label>
+                                    vendedor
+                                    <input type='radio' name='account-type' onClick={() => setVendor(true)} value='true'></input>
+                                </label>
+                            </div>
+                        </div>
+                        {
+                            isVendor &&
+                            <input type='text' name='CNPJ' className='cnpj-input' placeholder='CNPJ'></input>
+                        }
+                        <input type='text' name='phone' className='phone-input' placeholder='Telefone'></input>
+                        <input type='text' name='whatsapp' className='whatsapp-input' placeholder='Whatsapp'></input>
 
-                    <input type='text' name='CEP' className='cep-input' placeholder='CEP'></input>
-                    <input type='text' name='state' className='state-input' placeholder='Estado'></input>
-                    <input type='text' name='city' className='city-input' placeholder='Cidade'></input>
-                    <input type='text' name='district' className='district-input' placeholder='Bairro'></input>
-                    <input type='text' name='street' className='street-input' placeholder='Rua'></input>
-                    <input type='text' name='number' className='number-input' placeholder='Número'></input>
-                    <input type='text' name='optional' className='optional-input' placeholder='Andar/Apartamento'></input>
+                        <input type='text' name='CEP' className='cep-input' placeholder='CEP'></input>
+                        <div className='location-div'>
+                            <input type='text' name='state' className='state-input' placeholder='Estado'></input>
+                            <input type='text' name='city' className='city-input' placeholder='Cidade'></input>
+                            <input type='text' name='district' className='district-input' placeholder='Bairro'></input>
+                        </div>
+                        <div className='location-div'>
+                            <input type='text' name='street' className='street-input' placeholder='Rua'></input>
+                            <input type='text' name='number' className='number-input' placeholder='Número'></input>
+                            <input type='text' name='optional' className='optional-input' placeholder='Andar/Apartamento'></input>
+                        </div>
+                    </div>
                 </form>
                 <div className='btn-div'>
                     <button className='submit-btn' onClick={() => postRegister()}>Submit</button>
