b490ec508fbc3545d4e785fff67ce21dc76d5c3b
Rewe <kewyn.ryan@gmail.com>
2021-07-15 12:03:42 -0300

    Fixed webpack targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+bundleConfig =
+{
+    
     mode: 'development',
     entry: 
     {
-        bundle: `${__dirname}/src/index.tsx`,
-        app: `${__dirname}/src/express/app.ts`
+        bundle: `${__dirname}/src/index.tsx`
     },
     output: {
         path: `${__dirname}/dist`,
@@ -72,4 +73,82 @@ module.exports = {
         "mongoose": "require('mongoose')",
         "express": "require('express')"
     }
-};
+}
+
+expressConfig =
+{
+    mode: 'development',
+    entry: 
+    {
+        app: `${__dirname}/src/express/app.ts`
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    target: 'node',
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        __dirname: false
+    },
+    module: 
+    {
+        rules: 
+        [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                          outputPath: 'res',
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: 
+    {
+        extensions: [ '.ts', '.tsx', '.js'],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    externals: {
+        "saslprep": "require('saslprep')",
+        "mongoose": "require('mongoose')",
+        "express": "require('express')"
+    }
+}
+
+module.exports = [
+    bundleConfig,
+    expressConfig
+]
